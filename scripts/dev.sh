d5e286c6f274c725dc19e73e3f6326c15acd0d05
Joel Shellman <36110034+mentics@users.noreply.github.com>
2026-09-18 12:33:56 -0700

    sync

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,6 +7,6 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 cargo build --profile dev-fast -p tod -p tod-cli
-cargo build -p tod-cli
+# cargo build -p tod-cli
 # cargo run -p tod -- "$@"
 cargo run --profile dev-fast -p tod -- "$@"
